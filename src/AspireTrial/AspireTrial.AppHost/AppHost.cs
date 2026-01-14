556662363c32063e502157742a68450ad38c29ec
Jan de Vries <jan@jan-v.nl>
2026-01-14 21:59:41 +0100

    Working Python in dashboard

--- src/AspireTrial/AspireTrial.AppHost/AppHost.cs
+++ src/AspireTrial/AspireTrial.AppHost/AppHost.cs
@@ -4,9 +4,18 @@ builder.AddAzureContainerAppEnvironment("env");
 
 var pythonBackend = builder.AddExecutable(
     "backend", "python", "../AspireBackendService", "-m", "uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"
-);
+)
+.WithHttpEndpoint(targetPort: 8000)
+.WithEnvironment("DEBUG", "true")
+.WithEnvironment("LOG_LEVEL", "info")
+.WithEnvironment(context =>
+{
+    // Dynamic environment variables
+    context.EnvironmentVariables["START_TIME"] = DateTimeOffset.UtcNow.ToString();
+});
 
 var apiService = builder.AddProject<Projects.AspireTrial_ApiService>("apiservice")
+    .WithReference(pythonBackend.GetEndpoint("http"))
     .WithHttpHealthCheck("/health");
 
 builder.AddProject<Projects.AspireTrial_Web>("webfrontend")
