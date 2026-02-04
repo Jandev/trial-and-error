0c11ffd327bc7bbff1b0cb4574bd20fa97c0ee27
Jan de Vries <jan@jan-v.nl>
2026-02-04 21:22:37 +0100

    Add environment variables and backend test

--- src/dotnet/AspireTrial.AppHost/AppHost.cs
+++ src/dotnet/AspireTrial.AppHost/AppHost.cs
@@ -14,8 +14,16 @@ var pythonBackend = builder.AddUvicornApp("backend", pythonDir + "/src", "aspire
 {
     // Dynamic environment variables
     context.EnvironmentVariables["START_TIME"] = DateTimeOffset.UtcNow.ToString();
-});
+})
+.WithEnvironment("AZURE_AI_PROJECT_ENDPOINT", "https://spp-smartpricing-dev-ai-foundry.services.ai.azure.com/api/projects/spp-smartpricing-dev-project")
+.WithEnvironment("AZURE_AI_MODEL_DEPLOYMENT_NAME", "gpt-4o-mini")
+;
 
+
+/*
+export AZURE_AI_PROJECT_ENDPOINT="https://<your-project>.services.ai.azure.com/api/projects/<project-id>"
+export AZURE_AI_MODEL_DEPLOYMENT_NAME="gpt-4o-mini"
+*/
 var apiService = builder.AddProject<Projects.AspireTrial_ApiService>("apiservice")
     .WithReference(pythonBackend)
     .WithHttpHealthCheck("/health");
