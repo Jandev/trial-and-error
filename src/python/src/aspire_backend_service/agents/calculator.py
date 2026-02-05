499ff2db85608edba02e50cb7cf1b7382e05ec0c
Jan de Vries <jan@jan-v.nl>
2026-02-05 12:30:10 +0100

    Agent with calculation tools

--- src/python/src/aspire_backend_service/agents/calculator.py
+++ src/python/src/aspire_backend_service/agents/calculator.py
@@ -1,3 +1,64 @@
+import asyncio
+import os
+from azure.identity.aio import AzureCliCredential
+from agent_framework.azure import AzureAIAgentClient, AzureAIAgentsProvider
+from azure.ai.projects.aio import AIProjectClient
+from agent_framework import ChatAgent
+from azure.ai.agents.aio import AgentsClient
+
 class calculator:
     async def run(self, question: str):
-        return question
+        answer = "I don't know"
+        async with(
+            AzureCliCredential() as credential,
+            AgentsClient(endpoint=os.environ["AZURE_AI_PROJECT_ENDPOINT"], credential=credential) as agents_client,
+            AzureAIAgentsProvider(credential=credential) as provider
+        ):
+            agent_instructions = """You are able to do calculations on questions asked.
+            Tools have been provided to do those calculations. Always use the tools to create
+            answers and specify which tools have been used to the user.
+            """
+
+            calculator_agent = await agents_client.create_agent(
+                model=os.environ["AZURE_AI_MODEL_DEPLOYMENT_NAME"],
+                name="CalculatorAgent",
+                instructions=agent_instructions,
+                # Adding tools over here yields in a `Object of type FunctionTool is not JSON serializable`-error.
+                # tools= [count_letters]
+            )
+
+            try:
+                agent = provider.as_agent(
+                    calculator_agent,
+                    tools= [
+                        count_letters,
+                        calculate_square_root
+                    ])
+
+                answer = await agent.run(question)
+            finally:
+                await agents_client.delete_agent(calculator_agent.id)
+
+        return answer
+
+from typing import Annotated
+from pydantic import Field
+from agent_framework import tool
+from math import sqrt
+
+# @tool(approval_mode="never_require")
+def count_letters(
+        character: Annotated[str, Field(description="The character that needs to be counted in the string.")],
+        phrase: Annotated[str, Field(description="The word or phrase that needs its characters counted.")],
+) -> int:
+    """Count the number of specified characters in a specific word or phrase"""
+    counted_characters = phrase.count(character)
+    return counted_characters
+
+# @tool(approval_mode="never_require")
+def calculate_square_root(
+        number: Annotated[float, Field(description="The number you want the square root to be calculated for.")]
+) -> float:
+    """Calculate the square root of the provided number and return it."""
+    square_root = sqrt(number)
+    return square_root
